991eb70ef60abd3d5a53b90933968a6f7f1f70e1
enx-code <77584605+enx-code@users.noreply.github.com>
2021-07-15 14:01:08 +0100

    code exercise 1
    it was on wrong repo

---
+++ codeExe1.py
@@ -0,0 +1,11 @@
+'''Collect input in KG'''
+kg_input = input("Enter weight in KG:")
+
+'''Convert to float'''
+kg_input = float(kg_input);
+
+'''Convert to LB 1kg = 2.205lb'''
+lb_output = kg_input*2.205
+
+'''output print the value'''
+print("Weight converted to LB ",lb_output)
