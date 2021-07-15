19e873fb9fda5159105e29400265630761ce0e76
enx-code <77584605+enx-code@users.noreply.github.com>
2021-07-15 13:40:19 +0100

    code exercise 2
    it was on wrong repo

---
+++ codeExe2.py
@@ -0,0 +1,8 @@
+def dividedby_op(a,b):
+    """Operation fonction"""
+    """If condition"""
+    if b == 0:
+      print("Error you cannot divided by 0. Please enter an appropriate number") 
+    else:
+      print("{0} / {1} = {2}".format(a,b,divided_res))
+      divided_res= a/b;
